7455d735007f831d7a1a07267406462ae8220278
VITA-R9 <jhj9144@gmail.com>
2023-04-29 23:18:49 +0900

    dark mode, font size control

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:qrscan/qrscan.dart';
 
+// flutter pub add qrscan
+// flutter pub add permission_handler
+// flutter pub add audio_video_progress_bar
 void main() {
   runApp(const MyApp());
 }
@@ -13,30 +18,75 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _output = "Empty Scan Code";
+  String _output = "No scanned QR value yet";
+  int mySize = 15;
+  static final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        backgroundColor: Colors.grey,
-        body: Builder(
-          builder: (context) {
-            return Center(
-              child: Text(_output, style: TextStyle(color: Colors.black),),
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => _scan(),
-          tooltip: 'scan',
-          child: const Icon(Icons.camera_alt),
-        ),
-      ),
+    return ValueListenableBuilder<ThemeMode>(
+      valueListenable: themeNotifier,
+      builder: (_, ThemeMode currentMode, __) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          darkTheme: ThemeData.dark(),
+          themeMode: currentMode,
+          home: Scaffold(
+            appBar: AppBar(
+              title: const Text('QR sample'),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    themeNotifier.value =
+                        themeNotifier.value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+                  },
+                  icon: Icon(
+                    themeNotifier.value == ThemeMode.light ? Icons.dark_mode : Icons.light_mode,
+                  ),
+                ),
+              ],
+            ),
+            body: Builder(
+              builder: (context) {
+                return Column(
+                  children: [
+                    const SizedBox(height: 50),
+                    const Text('Text Size Control'),
+                    SizedBox(
+                      width: 300,
+                      child: ProgressBar(
+                        progress: Duration(minutes: mySize),
+                        total: const Duration(minutes: 30),
+                        onSeek: (percent) {
+                          setState(() {
+                            mySize = percent.inMinutes;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                    Center(
+                        child: Text(
+                      _output,
+                      style: TextStyle(fontSize: mySize.toDouble()),
+                    )),
+                  ],
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => _scan(),
+              tooltip: 'scan',
+              child: const Icon(Icons.camera_alt),
+            ),
+          ),
+        );
+      },
     );
   }
 
   Future _scan() async {
+    await Permission.camera.request();
     String? barcode = await scan();
     setState(() {
       _output = barcode!;
